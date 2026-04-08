cfadea1731b497fbd87efb2d5b4ebbfdf57157c1
Omardkck <107371206+Omardkck@users.noreply.github.com>
2026-04-08 13:06:29 +0100

    Chore: Sweep obsolete testing scripts from the repository to clean up tech debt

--- tests/scratch_test.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import edge_tts
-
-async def synthesize(text, output_file):
-    communicate = edge_tts.Communicate(text, "en-US-AriaNeural")
-    await communicate.save(output_file)
-
-asyncio.run(synthesize("Hello world", "test.mp3"))
